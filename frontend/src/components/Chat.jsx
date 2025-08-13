94c23cf40c77aba715bcdf035b5cddf40e37d301
Manoj Hegde <manojnhegde@gmail.com>
2025-08-13 16:32:27 +0530

    chnaged url

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -19,7 +19,7 @@ export default function Chat() {
     setLoading(true);
 
     try {
-      const res = await fetch(" http://127.0.0.1:8000/ask", {
+      const res = await fetch("https://my-portfolio-fhyq.onrender.com/ask", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ question: input }),
