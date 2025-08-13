4fcc7ca3a1013314cfd45168704cfe32dbf7e6ca
Manoj Hegde <manojnhegde@gmail.com>
2025-08-13 19:38:50 +0530

    updated contact

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -34,8 +34,7 @@ const Contact = () => {
   const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
   const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
   const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
-console.log("env finded")
-console.log(SERVICE_ID, TEMPLATE_ID, PUBLIC_KEY);
+
 
 
 
