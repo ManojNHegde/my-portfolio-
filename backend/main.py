983e324ce235119355a3129828518d852cb2eb43
Manoj Hegde <manojnhegde@gmail.com>
2025-08-13 16:41:04 +0530

    chnaged

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,6 @@ app = FastAPI(title="RAG Chat API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173","https://my-portfolio-xi-one-25.vercel.app"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
